331787644b1ebdf634a1ff1c9ed742040703b529
Ricardo Vallejo <ricardovallejo@seven4n.com>
2021-03-19 16:14:10 -0500

    Initial files to work with cucumber and cypress

---
+++ cypress/integration/formsTest.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+context("Test Forms Page", () => {
+    it("Explore Forms Page", () => {
+        cy.visit('https://ultimateqa.com/filling-out-forms/')
+    })
+})
