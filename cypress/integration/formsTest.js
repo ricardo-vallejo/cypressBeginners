6816dc25ad8d9087c902806f9c184bf95a14a115
Ricardo Vallejo <ricardovallejo@seven4n.com>
2021-03-25 11:33:58 -0500

    Removed all the previous tests

--- cypress/integration/formsTest.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-
-context("Test Forms Page", () => {
-    it("Explore Forms Page", () => {
-        cy.visit('https://ultimateqa.com/filling-out-forms/')
-    })
-})
